14b610dcf0e078380fd42a2e5442a124603dd77f
Estevan Machry <estevanmachry@gmail.com>
2023-07-26 19:38:23 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -17,7 +17,7 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
-	private static void printPiece(ChessPiece piece ) {
+	private static void printPiece(ChessPiece piece) {
 		if (piece == null)
 			System.out.print("-");
 		else
